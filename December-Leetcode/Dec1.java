bc9bdfbd8ab3af48c8c002683cee0df444c2d3ac
Shiv Arora <passionshiv007@gmail.com>
2024-12-03 18:05:58 +0530

    1346. Check If N and Its Double Exist

---
+++ December-Leetcode/Dec1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i =0; i < arr.length; i++){
+            if(map.containsKey(2*arr[i]) ||(arr[i] % 2 == 0 && map.containsKey(arr[i]/2))){
+                return true;
+            }
+            else{
+                map.put(arr[i], i);
+            }
+        }
+        return false;
+    }
+}
